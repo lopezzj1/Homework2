e3536c5ad1ccd40e4fa18e24e63316c5ec658068
lopezzj1 <lopezzj1@uw.edu>
2015-10-11 16:13:48 -0700

    Initial Commit

---
+++ Homework2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Homework2
+//
+//  Created by Jill Lopez on 10/11/15.
+//  Copyright © 2015 Jill Lopez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
